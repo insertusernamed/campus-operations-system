9b29dc78399520641b82e82b5e00e4728646218b
insertusernamed <yevtushenkodan04@gmail.com>
2026-01-14 13:32:15 -0500

    feat(backend): initialize spring boot project

---
+++ campus-scheduler-backend/src/main/java/org/campusscheduler/CampusSchedulerApplication.java
@@ -0,0 +1,13 @@
+package org.campusscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusSchedulerApplication.class, args);
+	}
+
+}
