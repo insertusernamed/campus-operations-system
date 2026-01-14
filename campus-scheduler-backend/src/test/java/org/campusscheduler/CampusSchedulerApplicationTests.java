67a3193dbcf3f32deeae18573a00e19f8529187d
insertusernamed <yevtushenkodan04@gmail.com>
2026-01-14 13:32:15 -0500

    feat(backend): initialize spring boot project

---
+++ campus-scheduler-backend/src/test/java/org/campusscheduler/CampusSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.campusscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
