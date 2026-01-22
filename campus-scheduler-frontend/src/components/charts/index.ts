9e1581d72f8d8c0804c101073fccb401a5daf861
insertusernamed <yevtushenkodan04@gmail.com>
2026-01-22 03:34:21 -0500

    feat(charts): add barrel export for chart components

---
+++ campus-scheduler-frontend/src/components/charts/index.ts
@@ -0,0 +1,6 @@
+export { default as BarChart } from './BarChart.vue'
+export { default as HeatMap } from './HeatMap.vue'
+export { default as StatCard } from './StatCard.vue'
+
+export type { BarChartData } from './BarChart.vue'
+export type { HeatmapCell } from './HeatMap.vue'
